de7246e762731d4354f324775f7c402d1ce892a9
tobber07 <tobber07@gmail.com>
2026-01-22 19:40:17 +0100

    Honse

---
+++ Animation.cs
@@ -0,0 +1,21 @@
+﻿namespace Honse;
+
+class Animation
+{
+    ///length of animation
+    private int length;
+    
+    
+    private int frame = 0;
+    private string[] frames;
+
+    /// <param name="frames">the path for the frames of the animation</param>
+    Animation(string[] frames)
+    {
+        this.frames = new string[frames.Length];
+        for (int i = 0; i < frames.Length; i++)
+        {
+            this.frames[i] = File.ReadAllText(frames[i]);
+        }
+    }
+}
---
+++ Horse.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace Honse;
+
+public class Horse
+{
+    private int x;
+    private int y;
+    private string name;
+    private Animation animation;
+
+    void MoveTo(int x, int y)
+    {
+        Clear();
+        this.x = x;
+        this.y = y;
+        Draw();
+    }
+
+    void Clear()
+    {
+        
+    }
+
+    void Draw()
+    {
+        
+    }
+}
